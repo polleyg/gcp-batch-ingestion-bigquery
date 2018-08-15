b591f22746bdfe0a026c5a42bc45c2330781e779
zippoobbiz <zippoobbiz@gmail.com>
2018-08-15 11:49:22 +1000

    update JS test to avoid remove WARNs

--- cloud-function/test-setup.test.js
+++ cloud-function/test-setup.test.js
@@ -1,6 +1,11 @@
 // test-setup.spec.js
 const sinon = require('sinon')
 const chai = require('chai')
+const sinonChai = require('sinon-chai')
+
+before(function () {
+  chai.use(sinonChai)
+})
 
 beforeEach(function () {
   this.sandbox = sinon.sandbox.create()
