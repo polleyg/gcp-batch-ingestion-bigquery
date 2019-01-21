2d9ae3178fe8e62308d305a05b2a837997cc979c
Graham Polley <polleyg@gmail.com>
2019-01-21 12:26:30 +1100

    SQL update

--- src/main/java/org/polleyg/TemplatePipeline.java
+++ src/main/java/org/polleyg/TemplatePipeline.java
@@ -5,12 +5,11 @@ import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.coders.RowCoder;
 import org.apache.beam.sdk.extensions.sql.SqlTransform;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
-import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.options.ValueProvider;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
@@ -21,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED;
-import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition.WRITE_APPEND;
+import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE;
 
 /**
  * Do some randomness
@@ -30,20 +29,19 @@ public class TemplatePipeline {
     public static final String HEADER = "year,month,day,wikimedia_project,language,title,views";
     public static final Schema SCHEMA = Schema.builder()
             .addStringField("wikimedia_project")
-            .addInt64Field("views")
+            .addInt32Field("views")
             .build();
 
     public static void main(String[] args) {
-        PipelineOptionsFactory.register(TemplateOptions.class);
-        TemplateOptions options = PipelineOptionsFactory
+        PipelineOptionsFactory.register(DataflowPipelineOptions.class);
+        DataflowPipelineOptions options = PipelineOptionsFactory
                 .fromArgs(args)
                 .withValidation()
-                .as(TemplateOptions.class);
-
+                .as(DataflowPipelineOptions.class);
         Pipeline pipeline = Pipeline.create(options);
 
         //READ
-        PCollection<String> read = pipeline.apply("gcs_read", TextIO.read().from(options.getInputFile()));
+        PCollection<String> read = pipeline.apply("gcs_read", TextIO.read().from("gs://batch-pipeline-sql/input.csv"));
 
         //BigQuery Rows (straight write)
         PCollection<TableRow> bigQueryRows = read.apply("csv_transform",
@@ -52,16 +50,17 @@ public class TemplatePipeline {
         //Sql Rows
         PCollection<Row> sqlRows = bigQueryRows.apply("row_transform",
                 ParDo.of(new RowParDo()));
-        sqlRows.setCoder(SCHEMA.getRowCoder());
+        sqlRows.setCoder(RowCoder.of(SCHEMA));
 
         //More Sql Rows
         PCollection<Row> outputStream =
-                sqlRows.apply("sql_transform",
-                        SqlTransform.query(
-                                "select wikimedia_project, sum(views) " +
-                                        "from PCOLLECTION " +
-                                        "group by wikimedia_project"));
-        outputStream.setCoder(SCHEMA.getRowCoder());
+                sqlRows.setRowSchema(SCHEMA)
+                        .apply("sql_transform",
+                                SqlTransform.query(
+                                        "select wikimedia_project, sum(views) " +
+                                                "from PCOLLECTION " +
+                                                "group by wikimedia_project"));
+        outputStream.setCoder(RowCoder.of(SCHEMA));
 
         //And back to BigQuery table rows *sigh*
         PCollection<TableRow> sqlAggregated = outputStream.apply("bq_row_transform",
@@ -69,15 +68,15 @@ public class TemplatePipeline {
 
 
         bigQueryRows.apply("raw_write_bq", BigQueryIO.writeTableRows()
-                .to(String.format("%s:dotc_2018.wiki_demo", options.getProject()))
+                .to(String.format("%s:beam_sql.full_output", options.getProject()))
                 .withCreateDisposition(CREATE_IF_NEEDED)
-                .withWriteDisposition(WRITE_APPEND)
+                .withWriteDisposition(WRITE_TRUNCATE)
                 .withSchema(getTableSchema()));
 
         sqlAggregated.apply("aggregated_write_bq", BigQueryIO.writeTableRows()
-                .to(String.format("%s:dotc_2018.wiki_demo_aggregated", options.getProject()))
+                .to(String.format("%s:beam_sql.sql_output", options.getProject()))
                 .withCreateDisposition(CREATE_IF_NEEDED)
-                .withWriteDisposition(WRITE_APPEND)
+                .withWriteDisposition(WRITE_TRUNCATE)
                 .withSchema(getTableSchemaAggregated()));
 
         pipeline.run();
@@ -102,15 +101,7 @@ public class TemplatePipeline {
         return new TableSchema().setFields(fields);
     }
 
-    public interface TemplateOptions extends DataflowPipelineOptions {
-        @Description("GCS path of the file to read from")
-        ValueProvider<String> getInputFile();
-
-        void setInputFile(ValueProvider<String> value);
-    }
-
     public static class WikiParDo extends DoFn<String, TableRow> {
-
         @ProcessElement
         public void processElement(ProcessContext c) {
             if (c.element().equalsIgnoreCase(HEADER)) return;
@@ -146,7 +137,10 @@ public class TemplatePipeline {
             TableRow bqRow = new TableRow();
 
             bqRow.set("wikimedia_project", row.getString("wikimedia_project"));
-            bqRow.set("views", row.getInt64("views"));
+            /**
+             * Looks like a bug here. I can't use "views" to get the value of the field.
+             */
+            bqRow.set("views", row.getInt32("EXPR$1"));
 
             c.output(bqRow);
         }
