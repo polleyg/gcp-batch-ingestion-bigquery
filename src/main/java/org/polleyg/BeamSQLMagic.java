ddff78085d5ce80510f926ff5f40d69a2be542ca
Graham Polley <polleyg@gmail.com>
2019-01-22 14:49:22 +1100

    update

--- src/main/java/org/polleyg/BeamSQLMagic.java
+++ src/main/java/org/polleyg/BeamSQLMagic.java
@@ -1,24 +1,14 @@
 package org.polleyg;
 
-import com.google.api.services.bigquery.model.TableFieldSchema;
-import com.google.api.services.bigquery.model.TableRow;
-import com.google.api.services.bigquery.model.TableSchema;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.extensions.sql.SqlTransform;
 import org.apache.beam.sdk.io.TextIO;
-import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.schemas.Schema;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.Row;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED;
-import static org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE;
 
 /**
  * Do some randomness
@@ -37,23 +27,12 @@ public class BeamSQLMagic {
                 .withValidation()
                 .as(DataflowPipelineOptions.class);
         Pipeline pipeline = Pipeline.create(options);
-        pipeline.apply("file_read", TextIO.read().from("gs://batch-pipeline-sql/input.csv"))
-                .apply("row_transform", ParDo.of(new RowParDo())).setRowSchema(SCHEMA)
-                .apply("sql_transform", SqlTransform.query("SELECT lang, SUM(views) FROM PCOLLECTION GROUP BY lang"))
-                .apply("to_bq_row_transform", ParDo.of(new RowToBigQueryRow()))
-                .apply("results_write", BigQueryIO.writeTableRows()
-                        .to(String.format("%s:beam_sql.sql_output", options.getProject()))
-                        .withCreateDisposition(CREATE_IF_NEEDED)
-                        .withWriteDisposition(WRITE_TRUNCATE)
-                        .withSchema(getTableSchemaAggregated()));
+        pipeline.apply("read_from_gcs", TextIO.read().from("gs://batch-pipeline-sql/input/*"))
+                .apply("transform_to_row", ParDo.of(new RowParDo())).setRowSchema(SCHEMA)
+                .apply("transform_sql", SqlTransform.query("SELECT SUM(sum_views) as total_views FROM (SELECT lang, SUM(views) as sum_views FROM PCOLLECTION GROUP BY lang)"))
+                .apply("transform_to_string", ParDo.of(new RowToString()))
+                .apply("write_to_gcs", TextIO.write().to("gs://batch-pipeline-sql/output/result.txt").withoutSharding());
         pipeline.run();
-    }
-
-    private static TableSchema getTableSchemaAggregated() {
-        List<TableFieldSchema> fields = new ArrayList<>();
-        fields.add(new TableFieldSchema().setName("lang").setType("STRING"));
-        fields.add(new TableFieldSchema().setName("views").setType("INTEGER"));
-        return new TableSchema().setFields(fields);
     }
 
     //ParDo for String -> Row (SQL)
@@ -64,21 +43,18 @@ public class BeamSQLMagic {
                 String[] vals = c.element().split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
                 Row appRow = Row
                         .withSchema(SCHEMA)
-                        .addValues(vals[4], Integer.valueOf(vals[6]))
+                        .addValues(vals[4], Integer.valueOf(vals[6])) //don't do this in prod!
                         .build();
                 c.output(appRow);
             }
         }
     }
 
-    //ParDo for Row (SQL) -> TableRow
-    public static class RowToBigQueryRow extends DoFn<Row, TableRow> {
+    //ParDo for Row (SQL) -> String
+    public static class RowToString extends DoFn<Row, String> {
         @ProcessElement
         public void processElement(ProcessContext c) {
-            TableRow bqRow = new TableRow()
-                    .set("lang", c.element().getString("lang"))
-                    .set("views", c.element().getInt32("EXPR$1")); //Beam bug?
-            c.output(bqRow);
+            c.output(c.element().getInt32("total_views").toString());
         }
     }
 }
